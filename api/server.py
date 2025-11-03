653fd5495ef00cb6ef639e2413eaeddb1afc7d49
FoolMasque <FoolMasque@gmail.com>
2025-11-03 17:43:17 +0800

    update

--- api/server.py
+++ api/server.py
@@ -181,8 +181,8 @@ async def health_check():
 
 def main():
     """主函数"""
-    # 配置
-    host = os.getenv("HOST", "localhost")
+    # TODO：需要从配置文件中读取
+    host = os.getenv("HOST", "0.0.0.0")
     port = int(os.getenv("PORT", "8008"))
     reload = os.getenv("RELOAD", "false").lower() == "true"
     workers = int(os.getenv("WORKERS", "1"))
